945af56862d6ba6c4e5d98aaddb29eb79625b054
Myh St <muyahid.dy@gmail.com>
2020-08-13 17:14:01 +0700

    update rdp2es

---
+++ rdp2es.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+NOW=$(date +"%m-%d-%Y_%Hh%Mm")
+> rdp-result/rdp2es/result.csv
+for HOST in `cat hosts.txt`
+do
+nc -z -v $HOST 3389
+if [ $? -eq 0 ]
+then
+echo "$HOST,SUCCESS" >> rdp-result/rdp-passed_$NOW.csv
+else
+echo "$HOST,FAILED" >> rdp-result/ssh-failed_$NOW.csv
+fi
+done
+cat rdp-result/rdp-passed_$NOW.csv rdp-result/rdp-failed_$NOW.csv > rdp-result/rdp2es/result.csv
--- ssh2es.sh
+++ ssh2es.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+NOW=$(date +"%m-%d-%Y_%Hh%Mm")
+> ssh-result/ssh2es/result.csv
 for HOST in `cat hosts.txt`
 do
 nc -z -v $HOST 22
 if [ $? -eq 0 ]
 then
- > ssh-passed.csv
-echo "$HOST,SUCCESS"
-echo "$HOST,SUCCESS" >> ssh-passed.csv
+echo "$HOST,SUCCESS" >> ssh-result/ssh-passed_$NOW.csv
 else
-echo "$HOST,FAILED"
-echo "$HOST,FAILED" >> ssh-failed.csv
+echo "$HOST,FAILED" >> ssh-result/ssh-failed_$NOW.csv
 fi
 done
+cat ssh-result/ssh-passed_$NOW.csv ssh-result/ssh-failed_$NOW.csv > ssh-result/ssh2es/result.csv
